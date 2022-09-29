fbba31d878587be942ae70f80cf0efa654d353f1
Andrii Shupta <andrii.shupta@gmail.com>
2022-09-29 11:24:36 +0300

    chore: publish

--- publish-mainnet.sh
+++ publish-mainnet.sh
@@ -1,5 +1,5 @@
 yarn run generate-api
 git add api && git commit -m "chore(ci): generate api for mainnet"
 yarn run build
-yarn run version:mainnet
-yarn run publish:mainnet
+yarn run mainnet:version
+yarn run mainnet:publish
--- publish-testnet.sh
+++ publish-testnet.sh
@@ -2,5 +2,5 @@ TESTNET="true"
 yarn run generate-api
 git add api && git commit -m "chore(ci): generate api for testnet"
 yarn run build
-yarn run version:testnet
-yarn run publish
+yarn run testnet:version
+yarn run testnet:publish
