4803c203ae52bc0033c37f744147dcbcd0a0418d
Andrii Shupta <andrii.shupta@gmail.com>
2022-11-20 12:48:07 +0200

    chore: update process of publishing

--- publish-mainnet.sh
+++ publish-mainnet.sh
@@ -1,5 +1,5 @@
 yarn run generate-api
-git add api && git commit -m "chore(ci): generate api for mainnet"
+git add api packages && git commit -m "chore(ci): generate api for mainnet"
 yarn run build
 yarn run mainnet:version
 yarn run mainnet:publish
--- publish-testnet.sh
+++ publish-testnet.sh
@@ -1,6 +1,6 @@
 export TESTNET="true"
 yarn run generate-api
-git add api && git commit -m "chore(ci): generate api for testnet"
+git add api packages && git commit -m "chore(ci): generate api for testnet"
 yarn run build
 yarn run testnet:version
 yarn run testnet:publish
