d637eb243c5a5aee938dc2e8bc795e9e66b72b34
Andrii Shupta <andrii.shupta@gmail.com>
2022-11-24 14:06:10 +0200

    fix(ci): fix publish commit order

--- publish-mainnet.sh
+++ publish-mainnet.sh
@@ -1,5 +1,5 @@
 yarn run generate-api
-git add api packages && git commit -m "chore(ci): generate api for mainnet"
 yarn run build
+git add api packages && git commit -m "chore(ci): generate api for mainnet"
 yarn run mainnet:version
 yarn run mainnet:publish
--- publish-testnet.sh
+++ publish-testnet.sh
@@ -1,6 +1,6 @@
 export TESTNET="true"
 yarn run generate-api
-git add api packages && git commit -m "chore(ci): generate api for testnet"
 yarn run build
+git add api packages && git commit -m "chore(ci): generate api for testnet"
 yarn run testnet:version
 yarn run testnet:publish
